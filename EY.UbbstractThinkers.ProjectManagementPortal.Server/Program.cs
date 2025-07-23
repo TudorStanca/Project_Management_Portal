ffa22c958f2e850f080199df8509f794e7a0a6d1
Adrian Vedinas <adrian.vedinas@ro.ey.com>
2025-07-23 10:36:24 +0300

    Add project files.

---
+++ EY.UbbstractThinkers.ProjectManagementPortal.Server/Program.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EY.UbbstractThinkers.ProjectManagementPortal.Server
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+
+            var app = builder.Build();
+
+            app.UseDefaultFiles();
+            app.MapStaticAssets();
+
+            // Configure the HTTP request pipeline.
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.MapFallbackToFile("/index.html");
+
+            app.Run();
+        }
+    }
+}
